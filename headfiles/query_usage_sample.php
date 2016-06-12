2d3dd616e6306b3a0c231b8c52fa6d46c11f8d49
Phoenix <phoenixsong@outlook.com>
2016-06-12 01:02:16 -0700

    Moved head files in to headfiles folder

---
+++ headfiles/query_usage_sample.php
@@ -0,0 +1,68 @@
+<?php
+/* 
+    Sample Query class usage demo
+
+	Author: Phoenix
+    Version: 0611.2016
+*/
+
+include_once "pdo_h.php";
+include_once "backend_classes_h.php";
+include_once "frontend_classes_h.php";
+
+// Execution codes:
+$q = new Query;
+echo "<h1>Search</h1>";
+$results = $q->search( '劉慈欣', 'eng');
+foreach($results as $row)
+	{
+		echo '<br>';
+		var_dump($row);
+		echo '<br>';
+
+		echo $row->toHTMLTableRow();
+	}
+
+
+echo "<h1>getBook</h1>";
+$obj = $q->getBook('The_Three_Body_Novels', 'zho');
+var_dump($obj);
+echo $obj->toHTMLDivision();
+
+echo "<h1>getAuthor</h1>";
+$obj = $q->getAuthor('Liu_Cixin', 'jpn');
+var_dump($obj);
+echo $obj->toHTMLDivision();
+
+
+echo "<h1>getBookLinks</h1>";
+$results =  $q->getBookLinks('The_Three_Body_Novels', 'eng');
+foreach($results as $row)
+	{
+		var_dump($row);
+		echo '<br /> <br />';
+
+		echo $row->toHTMLTableRow();
+	}
+
+
+echo "<h1>getBookComments</h1>";
+$results =  $q->getBookComments('The_Three_Body_Novels');
+foreach($results as $row)
+	{
+		echo '<br>';
+		var_dump($row);
+		echo '<br>';
+		echo $row->toHTMLTableRow();
+	}
+
+echo "<h1>getAuthorComments</h1>";
+$results =  $q->getAuthorComments('Liu_Cixin');
+foreach($results as $row)
+	{
+		echo '<br>';
+		var_dump($row);
+		echo '<br>';
+		echo $row->toHTMLTableRow();
+	}
+?>
